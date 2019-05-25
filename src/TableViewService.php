0f516973b1a8e6226e40afbaf8900c453a1c7fe0
BacLuc <lucius.bachmann@gmx.ch>
2019-05-25 18:30:43 +0200

    TableViewService: add mock implementation

--- src/TableViewService.php
+++ src/TableViewService.php
@@ -4,9 +4,15 @@
 namespace BasicTablePackage;
 
 
+use BasicTablePackage\View\TableView\Row;
 use BasicTablePackage\View\TableView\TableView;
 
 class TableViewService
 {
-    public function getTableView() : TableView {}
+    const HEADER_2 = "header2";
+    public function getTableView() : TableView {
+        $row1 = new Row([ "test1", "test2" ]);
+        $row2 = new Row([ "test3", "test4" ]);
+        return new TableView([ "header1", "header1" ], [ $row1, $row2]);
+    }
 }
