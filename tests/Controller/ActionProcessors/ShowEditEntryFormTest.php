c964b89037014ad20c1c65e13fb398487de498d1
Lucius Bachmann <lucius.bachmann@gmx.ch>
2019-10-08 09:34:25 +0200

    BasicTableControllerTest: move edit_existing_entry_test own Test

---
+++ tests/Controller/ActionProcessors/ShowEditEntryFormTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BasicTablePackage\Test\Controller\ActionProcessors;
+
+
+use BasicTablePackage\Controller\ActionRegistryFactory;
+use BasicTablePackage\Controller\BasicTableController;
+use BasicTablePackage\Test\DIContainerFactory;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+
+class ShowEditEntryFormTest extends TestCase
+{
+    const TEST_1 = "test_value";
+    /**
+     * @var BasicTableController
+     */
+    private $basicTableController;
+
+    protected function setUp()
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->createMock(EntityManager::class);
+        /** @var Container $container */
+        $this->basicTableController = DIContainerFactory::createContainer($entityManager)->get(BasicTableController::class);
+        $this->basicTableController->getActionFor(ActionRegistryFactory::POST_FORM)->process([], ["value" => self::TEST_1]);
+    }
+
+    public function test_edit_form_shows_value_of_existing_entry()
+    {
+        ob_start();
+        $this->basicTableController->getActionFor(ActionRegistryFactory::EDIT_ROW_FORM)
+            ->process([], [], 1);
+
+        $output = ob_get_clean();
+        $this->assertThat($output, $this->stringContains("value"));
+        $this->assertThat($output, $this->stringContains(self::TEST_1));
+        $this->assertThat($output, $this->stringContains("action=\"post_form/1\""));
+    }
+}
