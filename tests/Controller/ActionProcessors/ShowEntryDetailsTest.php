8f53f8ecad5d68a02097e60e02438093e4b8a1e8
Lucius Bachmann <lucius.bachmann@gmx.ch>
2019-10-08 09:39:53 +0200

    BasicTableControllerTest: move test_delete_entry to DeleteEntryTest

---
+++ tests/Controller/ActionProcessors/ShowEntryDetailsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BasicTablePackage\Test\Controller\ActionProcessors;
+
+
+use BasicTablePackage\Controller\ActionRegistryFactory;
+use BasicTablePackage\Controller\BasicTableController;
+use BasicTablePackage\Test\DIContainerFactory;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+
+class ShowEntryDetailsTest extends TestCase
+{
+    const TEST_1 = "test_value";
+    /**
+     * @var BasicTableController
+     */
+    private $basicTableController;
+
+    protected function setUp()
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->createMock(EntityManager::class);
+        /** @var Container $container */
+        $this->basicTableController = DIContainerFactory::createContainer($entityManager)->get(BasicTableController::class);
+        $this->basicTableController->getActionFor(ActionRegistryFactory::POST_FORM)->process([], ["value" => self::TEST_1]);
+    }
+
+    public function test_show_details()
+    {
+        ob_start();
+        $this->basicTableController->getActionFor(ActionRegistryFactory::SHOW_ENTRY_DETAILS)
+            ->process([], [], 1);
+
+        $output = ob_get_clean();
+        $this->assertThat($output, $this->stringContains("value"));
+        $this->assertThat($output, $this->stringContains(self::TEST_1));
+        $this->assertStringNotContainsString("<form", $output);
+    }
+
+}
