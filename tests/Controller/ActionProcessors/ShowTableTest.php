970511780b7a7d3f3bbdbdcd7f44615d0fcbca73
Lucius Bachmann <lucius.bachmann@gmx.ch>
2019-10-08 10:05:09 +0200

    BasicTableControllerTest: move show_table test to own class
    And delete BasicTableControllerTest.php

---
+++ tests/Controller/ActionProcessors/ShowTableTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BasicTablePackage\Test\Controller\ActionProcessors;
+
+
+use BasicTablePackage\Controller\ActionRegistryFactory;
+use BasicTablePackage\Controller\BasicTableController;
+use BasicTablePackage\Test\DIContainerFactory;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+
+class ShowTableTest extends TestCase
+{
+    const TEST_1 = "test1";
+    const TEST_2 = "test2";
+    const TEST_3 = "test3";
+    const TEST_4 = "test4";
+
+
+    private $basicTableController;
+
+
+    protected function setUp()
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->createMock(EntityManager::class);
+
+        /** @var Container $container */
+        $this->basicTableController = DIContainerFactory::createContainer($entityManager)->get(BasicTableController::class);
+        collect([self::TEST_1, self::TEST_2, self::TEST_3, self::TEST_4])->each(function (string $value) {
+            $this->basicTableController->getActionFor(ActionRegistryFactory::POST_FORM)->process([], ["value" => $value]);
+        });
+    }
+
+    public function test_sets_headers_and_rows_to_TableView_retrieved_from_TableViewService()
+    {
+        ob_start();
+        $this->basicTableController->getActionFor(ActionRegistryFactory::SHOW_TABLE)->process([], []);;
+
+        $output = ob_get_clean();
+        $this->assertThat($output, $this->stringContains("value"));
+        $this->assertThat($output, $this->stringContains(self::TEST_1));
+        $this->assertThat($output, $this->stringContains(self::TEST_2));
+        $this->assertThat($output, $this->stringContains(self::TEST_3));
+        $this->assertThat($output, $this->stringContains(self::TEST_4));
+    }
+}
