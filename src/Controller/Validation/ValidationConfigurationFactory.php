66c7c77266df3a64c584e6ac633617f8ae01f18f
Lucius Bachmann <lucius.bachmann@gmx.ch>
2019-10-17 08:36:38 +0200

    cosmetics/ValidationConfigurationFactory: auto format

--- src/Controller/Validation/ValidationConfigurationFactory.php
+++ src/Controller/Validation/ValidationConfigurationFactory.php
@@ -8,11 +8,11 @@ class ValidationConfigurationFactory
 {
     public function __construct () { }
 
-    public function createConfiguration (): ValidationConfiguration
+    public function createConfiguration(): ValidationConfiguration
     {
         return new ValidationConfiguration([
-                                               new TextFieldValidator("value"),
+            new TextFieldValidator("value"),
             new IntegerFieldValidator("intcolumn")
-                                           ]);
+        ]);
     }
 }
