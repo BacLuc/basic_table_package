193e2f290ef7cafb4ab3ef7d4cfe673e49c14bf6
Lucius Bachmann <lucius.bachmann@gmx.ch>
2019-10-18 11:39:02 +0200

    cosmetics/ExampleEntityConstants: reformat

--- tests/Controller/ActionProcessors/ExampleEntityConstants.php
+++ tests/Controller/ActionProcessors/ExampleEntityConstants.php
@@ -7,8 +7,12 @@ namespace BasicTablePackage\Test\Controller\ActionProcessors;
 class ExampleEntityConstants
 {
 
-    public const ENTRY_1_POST = ["value" => ExampleEntityConstants::TEXT_VAL_1, "intcolumn" => ExampleEntityConstants::INT_VAL_1, "datecolumn" => ExampleEntityConstants::DATE_VALUE_1];
     public const INT_VAL_1 = 65498;
     public const TEXT_VAL_1 = "test_value";
     public const DATE_VALUE_1 = '2020-12-12';
+    public const ENTRY_1_POST = [
+        "value" => self::TEXT_VAL_1,
+        "intcolumn" => self::INT_VAL_1,
+        "datecolumn" => self::DATE_VALUE_1
+    ];
 }
