7f3c2ee8a370bc1faf00c6528f81d1002a023cfe
BacLuc <lucius.bachmann@gmx.ch>
2019-10-20 11:19:16 +0200

    ValidationConfigurationFactory: use FieldTypeReader

--- src/Controller/Validation/ValidationConfigurationFactory.php
+++ src/Controller/Validation/ValidationConfigurationFactory.php
@@ -4,17 +4,49 @@
 namespace BasicTablePackage\Controller\Validation;
 
 
+use BasicTablePackage\Entity\PersistenceFieldTypeReader;
+use BasicTablePackage\Entity\PersistenceFieldTypes;
+use function BasicTablePackage\Lib\collect as collect;
+
 class ValidationConfigurationFactory
 {
-    public function __construct () { }
+    /**
+     * @var PersistenceFieldTypeReader
+     */
+    private $persistenceFieldTypeReader;
 
-    public function createConfiguration(): ValidationConfiguration
+    /**
+     * TableViewConfigurationFactory constructor.
+     * @param PersistenceFieldTypeReader $persistenceFieldTypeReader
+     */
+    public function __construct (PersistenceFieldTypeReader $persistenceFieldTypeReader)
     {
-        return new ValidationConfiguration([
-            new TextFieldValidator("value"),
-            new IntegerFieldValidator("intcolumn"),
-            new DateValidator("datecolumn"),
-            new DateValidator("datetimecolumn")
-        ]);
+        $this->persistenceFieldTypeReader = $persistenceFieldTypeReader;
+    }
+
+    public function createConfiguration (): ValidationConfiguration
+    {
+        $fieldTypes =
+            collect($this->persistenceFieldTypeReader->getPersistenceFieldTypes())
+                ->map(function ($persistenceFieldType, $key) {
+                    return self::createFieldTypeOf($persistenceFieldType,
+                                                   $key);
+                });
+        return new ValidationConfiguration($fieldTypes->toArray());
+    }
+
+    private function createFieldTypeOf (string $persistenceFieldType, string $key)
+    {
+        switch ($persistenceFieldType) {
+            case PersistenceFieldTypes::STRING:
+                return new TextFieldValidator($key);
+            case PersistenceFieldTypes::INTEGER:
+                return new IntegerFieldValidator($key);
+            case PersistenceFieldTypes::DATE:
+            case PersistenceFieldTypes::DATETIME:
+                return new DateValidator($key);
+            default:
+                return null;
+        }
     }
 }
