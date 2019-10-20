cd18fdd2e1fff4c4059b13615c6287e0e13e8e90
BacLuc <lucius.bachmann@gmx.ch>
2019-10-20 11:25:23 +0200

    PersistorConfigurationFactory: use PersistenceFieldTypeReader

--- src/Controller/ValuePersisters/PersistorConfigurationFactory.php
+++ src/Controller/ValuePersisters/PersistorConfigurationFactory.php
@@ -4,19 +4,49 @@
 namespace BasicTablePackage\Controller\ValuePersisters;
 
 
+use BasicTablePackage\Entity\PersistenceFieldTypeReader;
+use BasicTablePackage\Entity\PersistenceFieldTypes;
+use function BasicTablePackage\Lib\collect as collect;
+
 class PersistorConfigurationFactory
 {
-    public function __construct()
+    /**
+     * @var PersistenceFieldTypeReader
+     */
+    private $persistenceFieldTypeReader;
+
+    /**
+     * TableViewConfigurationFactory constructor.
+     * @param PersistenceFieldTypeReader $persistenceFieldTypeReader
+     */
+    public function __construct (PersistenceFieldTypeReader $persistenceFieldTypeReader)
     {
+        $this->persistenceFieldTypeReader = $persistenceFieldTypeReader;
     }
 
-    public function createConfiguration(): PersistorConfiguration
+    public function createConfiguration (): PersistorConfiguration
     {
-        return new PersistorConfiguration([
-            new TextFieldPersistor("value"),
-            new TextFieldPersistor("intcolumn"),
-            new DatePersistor("datecolumn"),
-            new DateTimePersistor("datetimecolumn")
-        ]);
+        $fieldTypes =
+            collect($this->persistenceFieldTypeReader->getPersistenceFieldTypes())
+                ->map(function ($persistenceFieldType, $key) {
+                    return self::createFieldTypeOf($persistenceFieldType,
+                                                   $key);
+                });
+        return new PersistorConfiguration($fieldTypes->toArray());
+    }
+
+    private function createFieldTypeOf (string $persistenceFieldType, string $key)
+    {
+        switch ($persistenceFieldType) {
+            case PersistenceFieldTypes::STRING:
+            case PersistenceFieldTypes::INTEGER:
+                return new TextFieldPersistor($key);
+            case PersistenceFieldTypes::DATE:
+                return new DatePersistor($key);
+            case PersistenceFieldTypes::DATETIME:
+                return new DateTimePersistor($key);
+            default:
+                return null;
+        }
     }
 }
