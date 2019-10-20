bb87f654ba6987bf75f60f65bbb5933da7a46c78
BacLuc <lucius.bachmann@gmx.ch>
2019-10-20 11:37:42 +0200

    FormViewConfigurationFactory: use PersistenceFieldTypeReader

--- src/View/FormView/FormViewConfigurationFactory.php
+++ src/View/FormView/FormViewConfigurationFactory.php
@@ -4,35 +4,63 @@
 namespace BasicTablePackage\View\FormView;
 
 
+use BasicTablePackage\Entity\PersistenceFieldTypeReader;
+use BasicTablePackage\Entity\PersistenceFieldTypes;
+use function BasicTablePackage\Lib\collect as collect;
+
 class FormViewConfigurationFactory
 {
 
     /**
-     * TableViewConfigurationFactory constructor.
+     * @var PersistenceFieldTypeReader
      */
-    public function __construct()
+    private $persistenceFieldTypeReader;
+
+    /**
+     * @param PersistenceFieldTypeReader $persistenceFieldTypeReader
+     */
+    public function __construct (PersistenceFieldTypeReader $persistenceFieldTypeReader)
     {
+        $this->persistenceFieldTypeReader = $persistenceFieldTypeReader;
     }
 
-    public function createConfiguration(): FormViewFieldConfiguration
+    public function createConfiguration (): FormViewFieldConfiguration
     {
-        return new FormViewFieldConfiguration([
-            "value" => function ($entity) {
-                $fieldName = "value";
-                return new TextField("value", "value", property_exists($entity, $fieldName) ? $entity->{$fieldName} : null);
-            },
-            "intcolumn" => function ($entity) {
-                $fieldName = "intcolumn";
-                return new IntegerField($fieldName, $fieldName, property_exists($entity, $fieldName) ? $entity->{$fieldName} : null);
-            },
-            "datecolumn" => function ($entity) {
-                $fieldName = "datecolumn";
-                return new DateField($fieldName, $fieldName, property_exists($entity, $fieldName) ? $entity->{$fieldName} : null);
-            },
-            "datetimecolumn" => function ($entity) {
-                $fieldName = "datetimecolumn";
-                return new DateTimeField($fieldName, $fieldName, property_exists($entity, $fieldName) ? $entity->{$fieldName} : null);
-            }
-        ]);
+        $fieldTypes =
+            collect($this->persistenceFieldTypeReader->getPersistenceFieldTypes())
+                ->map(function ($persistenceFieldType, $key) {
+                    return self::createFieldTypeOf($persistenceFieldType,
+                                                   $key);
+                });
+        return new FormViewFieldConfiguration($fieldTypes->toArray());
+    }
+
+    private function createFieldTypeOf (string $persistenceFieldType, string $key)
+    {
+        switch ($persistenceFieldType) {
+            case PersistenceFieldTypes::STRING:
+                return function ($entity) use ($key) {
+                    return new TextField($key, $key, self::extractSqlValueOfEntity($entity, $key));
+                };
+            case PersistenceFieldTypes::INTEGER:
+                return function ($entity) use ($key) {
+                    return new IntegerField($key, $key, self::extractSqlValueOfEntity($entity, $key));
+                };
+            case PersistenceFieldTypes::DATE:
+                return function ($entity) use ($key) {
+                    return new DateField($key, $key, self::extractSqlValueOfEntity($entity, $key));
+                };
+            case PersistenceFieldTypes::DATETIME:
+                return function ($entity) use ($key) {
+                    return new DateTimeField($key, $key, self::extractSqlValueOfEntity($entity, $key));
+                };
+            default:
+                return null;
+        }
+    }
+
+    private static function extractSqlValueOfEntity ($entity, string $key)
+    {
+        return property_exists($entity, $key) ? $entity->{$key} : null;
     }
 }
