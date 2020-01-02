ac7a288487e9dc89c099518d565e58e38cb3aba8
BacLuc <lucius.bachmann@gmx.ch>
2020-01-02 18:08:18 +0100

    Validation: add SelectMultipleFieldValidator

---
+++ src/Controller/Validation/SelectMultipleFieldValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace BasicTablePackage\Controller\Validation;
+
+
+use BasicTablePackage\Entity\ValueSupplier;
+use function BasicTablePackage\Lib\collect as collect;
+
+class SelectMultipleFieldValidator implements FieldValidator
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var ValueSupplier
+     */
+    private $valueSupplier;
+
+    /**
+     * TextFieldValidator constructor.
+     * @param string $name
+     * @param ValueSupplier $valueSupplier
+     */
+    public function __construct(string $name, ValueSupplier $valueSupplier)
+    {
+        $this->name = $name;
+        $this->valueSupplier = $valueSupplier;
+    }
+
+    public function validate($post): ValidationResultItem
+    {
+        $postValue = key_exists($this->name, $post) ? $post[$this->name] : null;
+        if ($postValue === "" || $postValue === null) {
+            $postValue = [];
+        }
+        if (!is_array($postValue)) {
+            return new ValidationResultItem($this->name, $postValue, ["the value must be an array"]);
+        }
+        $possibleValues = $this->valueSupplier->getValues();
+        $areAllValuesValid = collect($postValue)
+                                 ->filter(function ($value) use ($possibleValues) {
+                                     return !isset($possibleValues[$value]);
+                                 })->count() == 0;
+        if ($postValue !== [] && !$areAllValuesValid) {
+            return new ValidationResultItem($this->name,
+                $postValue,
+                [
+                    "One of the values sent [" .
+                    implode(", ", $postValue) .
+                    "] is not in the range of possible values. Possible are: [" .
+                    implode(",", $possibleValues) .
+                    "]"
+                ]);
+        }
+        return new ValidationResultItem($this->name, $postValue, []);
+    }
+
+}
---
+++ tests/Controller/Validation/SelectMultipleFieldValidatorTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BasicTablePackage\Test\Controller\Validation;
+
+use BasicTablePackage\Controller\Validation\SelectMultipleFieldValidator;
+use BasicTablePackage\Entity\ValueSupplier;
+use PHPUnit\Framework\TestCase;
+
+class SelectMultipleFieldValidatorTest extends TestCase
+{
+    private $valueSupplier;
+    private $dropdownFieldValidator;
+
+    function setUp()
+    {
+        $this->valueSupplier = $this->createMock(ValueSupplier::class);
+        $this->dropdownFieldValidator = new SelectMultipleFieldValidator("test", $this->valueSupplier);
+    }
+
+    public function test_fails_for_unknown_value()
+    {
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => "unkown"])->isError(), $this->isTrue());
+    }
+
+    public function test_return_valid_for_null()
+    {
+        $this->assertThat($this->dropdownFieldValidator->validate([])->isError(), $this->isFalse());
+    }
+
+    public function test_return_valid_for_empty_string()
+    {
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => ""])->isError(), $this->isFalse());
+    }
+
+    public function test_fail_for_unkown_0()
+    {
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => "0"])->isError(), $this->isTrue());
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => 0])->isError(), $this->isTrue());
+    }
+
+    public function test_return_valid_for_empty_array()
+    {
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => []])->isError(), $this->isFalse());
+    }
+
+    public function test_return_valid_for_known_value()
+    {
+        $knownKey = "knownkey";
+        $this->valueSupplier->expects($this->once())
+                            ->method("getValues")
+                            ->willReturn([$knownKey => "knownvalue"]);
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => [$knownKey]])->isError(),
+            $this->isFalse());
+    }
+
+    public function test_return_valid_for_known_values()
+    {
+        $knownKey = "knownkey";
+        $knownKey2 = "knownkey2";
+        $this->valueSupplier->expects($this->once())
+                            ->method("getValues")
+                            ->willReturn([$knownKey => "knownvalue", $knownKey2 => "knownvalue2"]);
+        $this->assertThat($this->dropdownFieldValidator->validate(["test" => [$knownKey, $knownKey2]])->isError(),
+            $this->isFalse());
+    }
+}
