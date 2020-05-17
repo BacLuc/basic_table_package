99cbef7668b0a865938dd64dc25add1e73791161
BacLuc <lucius.bachmann@gmx.ch>
2020-05-17 12:15:16 +0200

    datetimefield.php: use combination of date + time field
    Because firefox does not support datetime-local field.

--- resources/formfields/datetimefield.php
+++ resources/formfields/datetimefield.php
@@ -1,3 +1,26 @@
-<?php /** @noinspection ALL */ ?>
-<input type="datetime-local" name="<?= $postname ?>" value="<?= $datetime_local_value ?>"
-       formattedvalue="<?= $sqlValue ?>">
+<?php /** @noinspection ALL */
+$uuid = str_replace(".", "", uniqid("", true));
+if ($sqlValue && sizeof(explode(" ", $sqlValue)) == 2) {
+    list($date, $time) = explode(" ", $sqlValue);
+} else {
+    $date = "";
+    $time = "";
+}
+?>
+<span id="<?= $uuid ?>">
+    <input class="datetime" type="hidden" name="<?= $postname ?>" value="<?= $sqlValue ?>"/>
+
+    <input class="date" type="date" value="<?= $date ?>"/>
+
+    <input class="time" type="time" value="<?= $time ?>"/>
+</span>
+
+<script type="text/javascript">
+    (function ($) {
+        $(function () {
+            $('#<?= $uuid ?> .date, #<?= $uuid ?> .time').change(function () {
+                $('#<?= $uuid ?> .datetime').val($('#<?= $uuid ?> .date').val() + " " + $('#<?= $uuid ?> .time').val());
+            })
+        })
+    })(jQuery)
+</script>
